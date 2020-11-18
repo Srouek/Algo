3f0a2444d45580260fb086f706bc9449ea11727d
Srouek <44054224+Srouek@users.noreply.github.com>
2020-11-18 11:21:29 +0100

    Add files via upload
    Premier push

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import pandas as pd
+import numpy as np 
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+st.title("Streamlit Crash course")
+st.header("Simple Header")
+st.subheader("Another sub header")
+st.sidebar.header("Example de Side Bar")
+st.sidebar.text("Hello")
+st.text("For a simple text")
+st.markdown("#### A Markdown ")
+
